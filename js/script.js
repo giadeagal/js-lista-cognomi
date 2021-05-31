3b53f53a7a2ab2246900195b07a5a9d940f1f76f
Gianluca <gl.galassini@gmail.com>
2021-05-31 13:57:37 +0200

    fine esercizio mail

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-document.getElementById("printer").innerHTML = "Hello World!";
+var user = prompt("Inserisci il tuo cognome")
+
+whiteList = ["nome.cognome@gmail.com", "prova@gmail.com", "prova@gmail.it", "gl.galassini@gmail.com", "boolean@booleanmail.com"];
+
+document.getElementById("printer").innerHTML = "accesso negato"
+
+for (i = 0; i < whiteList.length; i++) {
+    if (user == whiteList[i]) {
+        document.getElementById("printer").innerHTML = "accesso autorizzato"
+    }
+}
