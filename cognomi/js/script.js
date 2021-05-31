7f6354c3a2b3d448a3e0d18ce798f8c5070a4c90
Gianluca <gl.galassini@gmail.com>
2021-05-31 16:06:40 +0200

    metodo con ciclo for

--- cognomi/js/script.js
+++ cognomi/js/script.js
@@ -5,10 +5,16 @@ whiteList = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 whiteList.push(user);
 
 whiteList = whiteList.sort();
+// Metodo con for
+for (i=0; i < whiteList.length ; i++) {
+    document.getElementById("printer").innerHTML += (i+1) + " "+whiteList[i] + " ";
+}
 
-document.getElementById("one").innerHTML = whiteList[0];
-document.getElementById("two").innerHTML = whiteList[1];
-document.getElementById("three").innerHTML = whiteList[2];
-document.getElementById("four").innerHTML = whiteList[3];
-document.getElementById("five").innerHTML = whiteList[4];
-document.getElementById("six").innerHTML = whiteList[5];
+
+// METODO PIù CARINO SENZA FOR
+// document.getElementById("one").innerHTML = whiteList[0];
+// document.getElementById("two").innerHTML = whiteList[1];
+// document.getElementById("three").innerHTML = whiteList[2];
+// document.getElementById("four").innerHTML = whiteList[3];
+// document.getElementById("five").innerHTML = whiteList[4];
+// document.getElementById("six").innerHTML = whiteList[5];
