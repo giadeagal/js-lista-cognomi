3a4253663422970a1c5110436ef2f51f5dd911e1
Gianluca <gl.galassini@gmail.com>
2021-05-31 13:58:13 +0200

    correzione prompt mail

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-var user = prompt("Inserisci il tuo cognome")
+var user = prompt("Inserisci la tua mail")
 
 whiteList = ["nome.cognome@gmail.com", "prova@gmail.com", "prova@gmail.it", "gl.galassini@gmail.com", "boolean@booleanmail.com"];
 
-document.getElementById("printer").innerHTML = "accesso negato"
+document.getElementById("printer").innerHTML = "accesso negato";
 
 for (i = 0; i < whiteList.length; i++) {
     if (user == whiteList[i]) {
         document.getElementById("printer").innerHTML = "accesso autorizzato"
     }
-}
+};
